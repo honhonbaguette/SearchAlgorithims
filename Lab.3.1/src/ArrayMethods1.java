df9e9cc3a82488256fc55d76fc270692fcb97f2c
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-01-18 09:23:21 -0500

    halp

--- Lab.3.1/src/ArrayMethods1.java
+++ Lab.3.1/src/ArrayMethods1.java
@@ -1,4 +1,48 @@
 
-public class ArrayMethods1 {
+public class ArrayMethods1 
+{
 
+	public static void main(String[] args)
+	{	
+		public static void insertionSort(int[] list1)
+		{
+			
+		}
+		
+		
+		
+		
+		public static void selectionSort(double [] list1)
+		{
+			
+		}
+		
+		
+		
+		public static void bubbleSort(String [] list1)
+		{
+			
+		}
+		
+			
+		public static void Swap(int[]array; int index1; int index2)
+		{
+			int temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;		
+		
+		
+		
+		
+		
+		
+		
+
+	private void int list1(int A, int b, int c, int d, int e, int f, int g, int h) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
